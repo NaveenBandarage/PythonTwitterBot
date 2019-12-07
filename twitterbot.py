9016c9c0efcf58c085e84763556a36b8ebf115f3
Naveen Bandarage <bandaragenaveen@gmail.com>
2019-12-07 15:44:35 +1300

    Added some basic text conversion functions

---
+++ twitterbot.py
@@ -0,0 +1,14 @@
+import random
+
+def textAlteration(text):
+
+    textArray = []
+    endText = '- omae wa mou shindeiru'
+for i in range(len(textArray)):
+    c =textArray[i]
+
+    
+
+def letter_position(letter):
+    ucase = string.uppercase
+    pos = ucase.find(letter.upper()) + 1
