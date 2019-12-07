f852fcfddae0f68ce624ca51814d31b7bd1d264d
Naveen Bandarage <bandaragenaveen@gmail.com>
2019-12-07 16:08:49 +1300

    Got the letter to number conversion working

--- twitterbot.py
+++ twitterbot.py
@@ -1,14 +1,25 @@
 import random
+#import string from ascii_lowercase
+
 
 def textAlteration(text):
-
     textArray = []
-    endText = '- omae wa mou shindeiru'
-for i in range(len(textArray)):
-    c =textArray[i]
+    numberString = ''
+    endText = ' - TextToNumbers'
+    for i in range(len(text)):
 
-    
+        numberString = str(ord(text[i].lower()) - 96)
+        print(numberString)
+        textArray.append(numberString)
 
-def letter_position(letter):
-    ucase = string.uppercase
-    pos = ucase.find(letter.upper()) + 1
+    return ''.join(textArray+ [endText])
+
+# def letter_position(letter):
+#     ucase = string.uppercase
+#     pos = ucase.find(letter.upper()) + 1
+#     return pos
+
+
+
+print(textAlteration("ABCD"))
+print(textAlteration("Hi my name is Naveen"))
